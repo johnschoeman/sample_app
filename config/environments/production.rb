245a42f9db3cb954dc0f37b2b13ecfb1e9773df1
John Schoeman <johnschoeman1617@gmail.com>
2016-02-18 01:51:57 +0000

    fix bug in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
-    :enable_strarttls_auto => true
+    :enable_starttls_auto => true
   }
 end
